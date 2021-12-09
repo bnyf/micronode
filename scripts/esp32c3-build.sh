8d01fd1a80db6b4a1cee10fad68044b67e9919d8
Yufeng Li <38353011+bnyf@users.noreply.github.com>
2021-12-09 17:02:39 +0800

    Delete esp32c3-build.sh

--- scripts/esp32c3-build.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "platforms/esp32/build" ]; then
-    mkdir ${MICRONODE}/platforms/esp32c3/build
-else
-    echo "build folder found"
-fi
-
-cmake ${MICRONODE}/platforms/esp32c3 -B platforms/esp32c3/build
-
-make -C ${MICRONODE}/platforms/esp32c3/build -j8
