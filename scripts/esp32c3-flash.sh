b7930a48ebc520dd08955f297d127e8485a0d17a
Yufeng Li <38353011+bnyf@users.noreply.github.com>
2021-12-09 17:03:11 +0800

    Delete esp32c3-flash.sh

--- scripts/esp32c3-flash.sh
+++
@@ -1,9 +0,0 @@
-# flash app
-make -C ${MICRONODE}/platforms/esp32c3/build flash ESPBAUD=1152000
-
-# flash 
-${MKSPIFFS}/mkspiffs -c ${MICRONODE}/framework -b 4096 -p 256 -s 0x200000 framework.bin
-python ${IDF_PATH}/components/esptool_py/esptool/esptool.py --chip esp32c3 --port /dev/ttyUSB0 --baud 1152000 write_flash -z 0x210000 framework.bin
-
-# monitor
-make monitor ESPBAUD=115200
